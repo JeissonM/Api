fc67ab62ae97c5fd35250011e86f26b954c75ad6
Jeisson Mandon <jorgejeisson@hotmail.com>
2019-03-12 19:21:06 -0500

    Versión 1.2 12 de Marzo 2019 19:04 Jeisson

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,21 @@
 use Illuminate\Http\Request;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('hola', 'UserController@index');
+    Route::get('inicio', 'CategoryController@index');
 });
